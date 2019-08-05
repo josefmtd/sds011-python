94abc71477c503b63f1775f38c70c67b782db8c6
Josef Matondang <josstemat@gmail.com>
2019-08-05 20:44:20 +0700

    Fixed SDS011 conversion

--- sds011.py
+++ sds011.py
@@ -5,18 +5,16 @@ import serial
 def parseSensor(sensorData):
     print("Received a continuous SDS011 packet")
     if compareCheckSum(sensorData[2:9]):
-        PM2_5 = ((ord(sensorData[3]) << 8 + ord(sensorData[2])))/10
-        PM10 = ((ord(sensorData[5]) << 8 + ord(sensorData[4])))/10
+        print(sensorData[2:4], 'and', sensorData[4:6])
+        PM2_5 = ( ord(sensorData[3]) * 256 + ord(sensorData[2]) )
+        PM10 = ( ord(sensorData[5]) * 256 + ord(sensorData[4]) )
         return (PM2_5, PM10)
     
 def compareCheckSum(sensorData):
     checkSum = 0
     for x in range(0, len(sensorData)-1):
         checkSum = checkSum + ord(sensorData[x])
-    print(len(sensorData))
     checkSum = checkSum & 255
-    print(checkSum)
-    print(ord(sensorData[-1]))
     return (checkSum == ord(sensorData[-1]))
 
 def main():
@@ -26,7 +24,8 @@ def main():
         sensorData = sds011.read(10) # Read 10 bytes from sensor
         print(len(sensorData))
         PM2_5, PM10 = parseSensor(sensorData)
-    
-        print('SDS-011 Reading: PM2.5 = %f and PM10 = %f'%(PM2_5, PM10))
+        PM2_5 = float(PM2_5)/10
+        PM10 = float(PM10)/10
+        print('SDS-011 Reading: PM2.5 = %.2f and PM10 = %.2f'%(PM2_5, PM10))
 
 main()
